bd75046fe4887f9619a9550cfcd35d651dcd65fc
AnwardeenM <96279461+AnwardeenM@users.noreply.github.com>
2022-04-03 23:13:14 +0530

    Create db.js

---
+++ src/configs/db.js
@@ -0,0 +1,5 @@
+const mongoose=require("mongoose");
+
+module.exports = ()=>{
+    return mongoose.connect("mongodb+srv://AnwardeenM:anwar1234@cluster0.mnqdo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+}
